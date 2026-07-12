cf856810b823f443a767d573b6f608bc3aca6017
GouravDevmx <morenabat230@gmail.com>
2026-07-11 21:17:19 -0600

    Update Supabase service role key in script

--- scripts/create-admin-no-deps.js
+++ scripts/create-admin-no-deps.js
@@ -1,5 +1,5 @@
 set SUPABASE_URL=https://kbdnecxnzcbtzqpfzvik.supabase.co
-set SUPABASE_SERVICE_ROLE_KEY=your-sb_secret-value-here
+set SUPABASE_SERVICE_ROLE_KEY= sb_secret_B97WLpE3CX77W3wn9FkJpA_BhNj0dVF
 
 node scripts\create-admin-no-deps.js --slug nails --email morenabat230@gmail.com  --password "Chambal@110"
 node scripts\create-admin-no-deps.js --slug makeup --email owner@glowmakeup.mx --password "choose-a-different-strong-password"
