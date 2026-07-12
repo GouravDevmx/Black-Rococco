9a899e7b82c6a159c272a278c12aa156a560af89
GouravDevmx <morenabat230@gmail.com>
2026-07-11 21:13:39 -0600

    Create create-admin-no-deps.js

---
+++ scripts/create-admin-no-deps.js
@@ -0,0 +1,5 @@
+set SUPABASE_URL=https://kbdnecxnzcbtzqpfzvik.supabase.co
+set SUPABASE_SERVICE_ROLE_KEY=your-sb_secret-value-here
+
+node scripts\create-admin-no-deps.js --slug nails --email morenabat230@gmail.com  --password "Chambal@110"
+node scripts\create-admin-no-deps.js --slug makeup --email owner@glowmakeup.mx --password "choose-a-different-strong-password"
